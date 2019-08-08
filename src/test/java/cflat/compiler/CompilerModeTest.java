dac8a717f1b1d2a529884d5b4e0fb9ed0458d16b
NAKAJIMA Yusaku <yupong7@gmail.com>
2019-08-09 01:44:43 +0900

    add Test

--- src/test/java/cflat/compiler/CompilerModeTest.java
+++ src/test/java/cflat/compiler/CompilerModeTest.java
@@ -1,12 +1,11 @@
 package cflat.compiler;
-/*
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.junit.hamcrest.core.ls.is;
-*/
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 public class CompilerModeTest {
-    //@Test
+    @Test
     public void testFoo() {
-	//assertThat(1+1, 2);
+	assertTrue(true);
     }
 }
