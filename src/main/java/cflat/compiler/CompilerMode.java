abd6f7764759cf3ffd3f6a2e0d4c03b438982496
NAKAJIMA Yusaku <yupong7@gmail.com>
2019-08-09 02:54:17 +0900

    add test.

--- src/main/java/cflat/compiler/CompilerMode.java
+++ src/main/java/cflat/compiler/CompilerMode.java
@@ -13,13 +13,42 @@ enum CompilerMode {
     static private Map<String, CompilerMode> modes;
     static {
 	modes = new HashMap<String, CompilerMode>();
-	modes.put("--check_syntax", CheckSyntax);
-	modes.put("--dump-tokens", DumpTokens);
+	/* modes.put(CheckSyntax.option, CheckSyntax);	
+	modes.put(DumpTokens.option, DumpTokens);
+	modes.put(DumpAST.option, DumpAST);
+	modes.put(DumpIR.option, DumpIR);
+	modes.put(DumpAsm.option, DumpAsm);*/
+	for(CompilerMode mode : CompilerMode.values()) {
+	    modes.put(mode.option, mode);
+	}
     }
 
     private final String option;
     
     CompilerMode(String option) {
 	this.option = option;
+    }
+
+    static public boolean isModeOption(String opt) {
+	for(CompilerMode mode : CompilerMode.values()) {
+	    if (opt.equals(mode.option))
+		return true;
+	}
+	return false;
+    }
+    
+    static public CompilerMode fromOption(String opt) {
+	for(CompilerMode mode : CompilerMode.values()) {
+	    if (opt.equals(mode.option))
+		return mode;
+	}
+	throw new Error("must not happen: unknown mode option: " + opt);
+    }
+    
+    public String toOption() {
+	return option;
+    }
+    boolean requires(CompilerMode m) {
+	return ordinal() >= m.ordinal();
     }
 }
--- src/test/java/cflat/compiler/CompilerModeTest.java
+++ src/test/java/cflat/compiler/CompilerModeTest.java
@@ -1,11 +1,29 @@
 package cflat.compiler;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CompilerModeTest {
     @Test
-    public void testFoo() {
+    public void testIsModeOption() {
+	CompilerMode m = CompilerMode.CheckSyntax;
+	assertTrue(CompilerMode.isModeOption(m.toOption()));
+	assertTrue(!CompilerMode.isModeOption(""));
+    }
+    @Test
+    public void testFromOption() {
+	CompilerMode m = CompilerMode.CheckSyntax;	
+	assertEquals("--check-syntax", m.toOption());
+    }
+    
+    @Test
+    public void testRequires() {
+	CompilerMode m0 = CompilerMode.CheckSyntax;
+	CompilerMode m1 = CompilerMode.DumpTokens;
+
+	assertTrue(m0.requires(m0));
+	assertTrue(!m0.requires(m1));		
 	assertTrue(true);
     }
 }
