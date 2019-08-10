23d6307b264caa2dee3a5d2e584ee5c050d4b3aa
NAKAJIMA Yusaku <yupong7@gmail.com>
2019-08-10 22:41:04 +0900

    implement term()

--- src/main/java/cflat/ast/ASTVisitor.java
+++ src/main/java/cflat/ast/ASTVisitor.java
@@ -2,8 +2,10 @@ package cflat.ast;
 
 public interface ASTVisitor<S,E> {
     // Statements
+
     // Expressions
     public E visit(BinaryOpNode node);
+    public E visit(CastNode node);
     public E visit(VariableNode node);    
     public E visit(IntegerLiteralNode node);
     public E visit(StringLiteralNode node);    
---
+++ src/main/java/cflat/ast/CastNode.java
@@ -0,0 +1,30 @@
+package cflat.ast;
+
+import cflat.type.Type;
+
+public class CastNode extends ExprNode {
+    protected TypeNode typeNode;
+    protected ExprNode expr;
+
+    public CastNode(Type t, ExprNode expr) {
+	this(new TypeNode(t), expr);
+    }
+
+    public CastNode(TypeNode t, ExprNode expr) {
+	this.typeNode = t;
+	this.expr = expr;
+    }
+    public Type type() {
+	return typeNode.type();
+    }
+    public Location location() {
+	return typeNode.location();
+    }
+    protected void _dump(Dumper d) {
+	d.printMember("typeNode", typeNode);
+	d.printMember("expr", expr);
+    }
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+	return visitor.visit(this);
+    }
+}
