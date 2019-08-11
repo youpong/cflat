95607956760b6cb706cabb80924aee5eb1db92c3
NAKAJIMA Yusaku <yupong7@gmail.com>
2019-08-11 15:15:55 +0900

    implement expr1()

---
+++ src/main/java/cflat/ast/PtrMemberNode.java
@@ -0,0 +1,46 @@
+package cflat.ast;
+
+import cflat.type.Type;
+
+/**
+ * ポインタ間接メンバ参照 ptr->memb
+ */
+public class PtrMemberNode extends LHSNode {
+    // TODO: private?
+    public ExprNode expr;
+    public String member;
+
+    public PtrMemberNode(ExprNode expr, String member) {
+	this.expr = expr;
+	this.member = member;
+    }
+    /*
+    public Type dereferencedType() {
+	try {
+	    PointerType pt = expr.type().getPointerType();
+	    return pt.baseType();
+	}
+	catch(ClassCastException err) {
+	    throw new SemanticError(err.getMessage());
+	}
+    }
+    */
+    // TODO: implement
+    protected Type origType(){
+	//return dereferencedCompositeType().memberType(member);
+	return null;
+    }
+
+    public Location location() {
+	return expr.location();
+    }
+    protected void _dump(Dumper d) {
+	if(type!=null)
+	    d.printMember("type", type);
+	d.printMember("expr", expr);
+	d.printMember("member", member);
+    }
+    public <S,E> E accept(ASTVisitor<S,E> visitor) {
+	return visitor.visit(this);
+    }
+}
