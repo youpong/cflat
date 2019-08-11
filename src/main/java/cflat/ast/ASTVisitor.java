61579b9557a8d7855a97ab517c43d30b4fc29dbe
NAKAJIMA Yusaku <yupong7@gmail.com>
2019-08-11 15:43:35 +0900

    implement expr8()

--- src/main/java/cflat/ast/ASTVisitor.java
+++ src/main/java/cflat/ast/ASTVisitor.java
@@ -11,6 +11,7 @@ public interface ASTVisitor<S,E> {
     public E visit(DereferenceNode node);
     public E visit(FuncallNode node);    
     public E visit(IntegerLiteralNode node);
+    public E visit(LogicalAndNode node);    
     public E visit(MemberNode node);
     public E visit(PrefixOpNode node);
     public E visit(PtrMemberNode node);    
