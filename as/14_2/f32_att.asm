cd9ce8b74c4bb76371055c04a501b9c8e2139ba0
NAKAJIMA Yusaku <yupong7@gmail.com>
2019-12-07 22:51:20 +0900

    pass assemble

--- as/14_2/f32_att.asm
+++ as/14_2/f32_att.asm
@@ -1,62 +1,66 @@
-; nasm x86 full assembly code
-; AT&T syntax 
+# gas x86 full assembly code
+# AT&T syntax
+# TODO: pass assemble
+	.global main
 f:
-	; variable table		
-	; x ->  8(%ebp), y -> 12(%ebp)
-	; i -> -4(%ebp), j -> -8(%ebp)
+	# variable table
+	# x ->  8(%ebp), y -> 12(%ebp)
+	# i -> -4(%ebp), j -> -8(%ebp)
 	
-	; prologue
+	# prologue
 	pushl	%ebp
 	movl	%esp, %ebp
 	subl	$8, %esp	
 	
-	; i = x
-	movl 	8(%ebp), -4(%ebp)
+	# i = x
+	#movl 	8(%ebp), -4(%ebp)
+	movl 	8(%ebp), %eax
+	movl	%eax, -4(%ebp)
 	
-	; j = i * y
+	# j = i * y
 	movl	12(%ebp), %eax
 	imull	-4(%ebp), %eax	
 	movl	%eax, -8(%ebp)
 		
-	; set eax to return value
+	# set eax to return value
 	movl	-8(%ebp), %eax
 	
-	; epilogue
+	# epilogue
 	movl	%ebp, %esp
 	popl	%ebp
 	ret
 
 main:
-	; variable table
-	; i -> -4(%ebp)
+	# variable table
+	# i -> -4(%ebp)
 	
-	; prologue
+	# prologue
 	pushl	%ebp
 	movl	%esp, %ebp
 	subl	$4, %esp
 	
-	; i = 77
+	# i = 77
 	movl	$77, -4(%ebp)	
 	
-	; i = f(i, 8)
-	pushl	$8		; push 2nd arg(8)
+	# i = f(i, 8)
+	pushl	$8		# push 2nd arg(8) 
 	movl	-4(%ebp), %eax
-	pushl	%eax		; push 1st arg(i)
-	call	f		; call f
-	addl	$8, %esp	; stack clear
-	movl	%eax, -4(%ebp)	; set i
+	pushl	%eax		# push 1st arg(i)
+	call	f		# call f
+	addl	$8, %esp	# stack clear
+	movl	%eax, -4(%ebp)	# set i
 
-	; i %= 5
+	# i %= 5
 	movl	$5, %ecx
 	movl	-4(%ebp), %eax
-	ctld			; signed expantion edx:eax from eax
-	idivl	%ecx		; signed eax/ecx -> quatient eax, reminder edx
-	movl	%edx, -4(%ebp)	; set i
+	cltd			# signed expantion edx:eax from eax
+	idivl	%ecx		# signed eax/ecx -> quatient eax, reminder edx
+	movl	%edx, -4(%ebp)	# set i
 	
-	; set return value of function
+	# set return value of function
 	movl	-4(%ebp), %eax
 	
-	; epilogue
+	# epilogue
 	movl	%ebp, %esp
 	popl	%esp
 	ret
